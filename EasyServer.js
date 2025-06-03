ae105b73356acde7884f87b260ece48834038433
Lyall <me@lyall.lol>
2025-06-03 06:43:32 +0100

    test

--- EasyServer.js
+++ EasyServer.js
@@ -19,8 +19,6 @@ class EasyServer {
             for (const currentFilePath of this.paths) {
                 filePath = path.join(currentFilePath, urlPath);
 
-                console.log(filePath, currentFilePath + urlPath);
-
                 if (filePath !== currentFilePath + urlPath) filePath = null;
                 if (!fs.existsSync(filePath)) filePath = null;
 
